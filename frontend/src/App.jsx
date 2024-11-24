811c261449f9c9791d9ab6a4788effe78ad006fb
Swapnil0809 <swapniljadhav22811@gmail.com>
2024-11-24 21:12:38 +0530

    added dashboards for users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup"
+import AdminDashboard from "./components/AdminDashboard"
+import CityAdminDashboard from "./components/CityAdminDashboard";
+import DonorDashboard from "./components/DonorDashboard";
+import RecipientDashboard from "./components/RecipientDashboard";
+
 
 function App() {
   return (
@@ -10,6 +15,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login/>} />
         <Route path="/signup" element={<Signup/>} />
+        <Route path="/admindashboard" element={<AdminDashboard/>} />
+        <Route path="/cityadmindashboard" element={<CityAdminDashboard/>} />
+        <Route path="/donordashboard" element={<DonorDashboard/>} />
+        <Route path="/recipientdashboard" element={<RecipientDashboard/>} />
+
+
       </Routes>
     </>
   );
